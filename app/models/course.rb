73ccee55b8108424ed44cb8a72a63bdd499b1906
Puja Kamath <puja.kamath@gmail.com>
2014-12-23 23:04:45 +0530

    add validations for Users and courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -9,5 +9,8 @@ class Course < ActiveRecord::Base
 	end 
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_presence :image
+  validates_presence_of :name, :description, :level, :duration, :format
+  validates_numericality_of :duration, :greater_than => 0
 
 end
