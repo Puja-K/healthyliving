3b4fae8310c5b779bbbb0dcbf11c5028245f4673
Puja Kamath <puja.kamath@gmail.com>
2015-02-02 11:25:51 +0530

    fixed heroku error for instructor model

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -2,7 +2,7 @@ class Instructor < ActiveRecord::Base
 	if Rails.env.development?
 		has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "defaults.jpeg"
   	else
-  		has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "default.jpeg",
+  		has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "default.jpeg",
 						:storage => :dropbox,
 	    				:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
 	    				:path => ":style/:id_:filename"
