ffa13a733feef4f5b2a9a8607c1e75f718303e10
Puja Kamath <puja.kamath@gmail.com>
2015-05-23 09:50:26 -0700

    added pagination on course details page, similar courses widget

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -22,7 +22,6 @@ class CoursesController < ApplicationController
   end
 
    def byCategory
-    sleep 2
       @courses = Course.where("category_id = ?", params[:category_id]).paginate(:page => params[:page], :per_page => 6)
       respond_to do |format|
         format.html {render 'byCategory'}
@@ -116,7 +115,7 @@ class CoursesController < ApplicationController
     def allcoursesbyinstructor
       @course = Course.find(params[:id])
       @courses = Course.where("instructor_id = ?", @course.instructor_id).paginate(:page => params[:page], :per_page => 3)
-
+      
     end
 
     helper_method :allcoursesbyinstructor
